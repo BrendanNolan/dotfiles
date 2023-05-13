48fcee3e96af4fbd5191133bceb29fa817794cc3
Brendan Nolan <bnolan23@gmail.com>
2023-05-13 13:46:25 +0100

    Wezterm: No Check Updates. No Warn Missing Glyphs.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,7 @@
 local wezterm = require 'wezterm'
 
 return {
+    check_for_updates = false,
     enable_scroll_bar = true,
     keys = {
         {
@@ -10,5 +11,6 @@ return {
     },
     font = wezterm.font 'Fira Code',
     hide_tab_bar_if_only_one_tab = true,
+    warn_about_missing_glyphs = false,
 }
 
