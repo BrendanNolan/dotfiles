5edd1949e346c20565565152dab035b0ea30efd6
Brendan Nolan <bnolan23@gmail.com>
2023-09-22 17:28:18 +0100

    Wezterm: Warn Abiut Missing Glyphs

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -12,6 +12,6 @@ return {
     font = wezterm.font 'Fira Code',
     font_size = 18.0,
     hide_tab_bar_if_only_one_tab = true,
-    warn_about_missing_glyphs = false,
+    warn_about_missing_glyphs = true,
 }
 
