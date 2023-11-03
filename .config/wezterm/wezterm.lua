2e1036c17cd5d99bd961fb124295a6bd7ac206a4
Brendan Nolan <bnolan23@gmail.com>
2023-11-03 07:49:11 +0000

    Make SHIFT+3 Put # In Wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -8,6 +8,11 @@ return {
             key = 'F11',
             action = wezterm.action.ToggleFullScreen,
         },
+        {  -- Make Wezterm Interpret SHIFT+3 as #
+            key = '£',  -- I'd expect to have 3 here but for whatever reason that doesn't work
+            mods = 'SHIFT',
+            action = wezterm.action.SendString '#',
+        },
     },
     font = wezterm.font 'Fira Code',
     font_size = 18.0,
