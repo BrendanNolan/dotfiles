863d6c5395c126571991dfea34f0be226f99b1cc
Brendan Nolan <bnolan23@gmail.com>
2025-07-08 06:04:48 +0100

    Split off fish alias file from main config file

---
+++ .config/fish/alias_abbr.fish
@@ -0,0 +1,23 @@
+## Ls
+alias l 'eza --icons'
+alias ll 'eza -l --icons'
+alias la 'eza -la --icons'
+alias ls 'eza --icons'
+## Git
+abbr --add g git
+abbr --add ge 'git add . && git status && sleep 1 && git diff --cached'
+abbr --add create-remote-repo 'gh repo create --source=. --private --push'
+abbr --add vis 'gh repo view --json visibility -q .visibility'
+## Copilot
+abbr --add cpe 'gh copilot explain'
+abbr --add cps 'gh copilot suggest'
+## Rust
+abbr --add cg cargo
+abbr --add fmt-all 'cargo +nightly fmt'
+abbr --add re 'echo "Formatting ..." ; fmt-all ; echo "Checking ..." ; cargo check ; echo "Clippying ..." ; cargo clippy'
+## Tmux
+abbr --add tka 'tmux kill-server'
+abbr --add tls 'tmux ls'
+abbr --add tm 'tmux'
+## Misc
+abbr nv 'nvim .'
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,34 +1,10 @@
-set -gx fish_greeting
-set -gx fish_color_command ebde34 # Set the color of text that I type on the command line
-set -gx fifc_editor nvim
-
-# Abbreviations/Aliases
-## Ls
-alias l 'eza --icons'
-alias ll 'eza -l --icons'
-alias la 'eza -la --icons'
-alias ls 'eza --icons'
-## Git
-abbr --add g git
-abbr --add ge 'git add . && git status && sleep 1 && git diff --cached'
-abbr --add create-remote-repo 'gh repo create --source=. --private --push'
-abbr --add vis 'gh repo view --json visibility -q .visibility'
-## Copilot
-abbr --add cpe 'gh copilot explain'
-abbr --add cps 'gh copilot suggest'
-## Rust
-abbr --add cg cargo
-abbr --add fmt-all 'cargo +nightly fmt'
-abbr --add re 'echo "Formatting ..." ; fmt-all ; echo "Checking ..." ; cargo check ; echo "Clippying ..." ; cargo clippy'
-## Tmux
-abbr --add tka 'tmux kill-server'
-abbr --add tls 'tmux ls'
-abbr --add tm 'tmux'
-## Misc
-abbr nv 'nvim .'
-
 if status is-interactive
-    # Set vi mode
+    set -gx fish_greeting
+    set -gx fish_color_command ebde34 # Set the color of text that I type on the command line
+    set -gx fifc_editor nvim
+
+    source $__fish_config_dir/alias_abbr.fish
+
     fish_vi_key_bindings
 
     bind --user -M insert \ck up-or-search
