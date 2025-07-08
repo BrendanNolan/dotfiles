ea3fbdd3911cd737358f87d3bf712c33e80d23e4
Brendan Nolan <bnolan23@gmail.com>
2025-07-08 05:59:33 +0100

    Clean up fish aliases/abbreviations

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,37 +2,30 @@ set -gx fish_greeting
 set -gx fish_color_command ebde34 # Set the color of text that I type on the command line
 set -gx fifc_editor nvim
 
-# Aliases
+# Abbreviations/Aliases
 ## Ls
 alias l 'eza --icons'
 alias ll 'eza -l --icons'
 alias la 'eza -la --icons'
 alias ls 'eza --icons'
-## Cd stuff
-alias cdf 'cd (fd -t d -d 8 | fzf)'
 ## Git
-alias g 'git'
-alias ge 'git add . && git status && sleep 1 && git diff --cached'
-alias create-remote-repo 'gh repo create --source=. --private --push'
-alias vis 'gh repo view --json visibility -q .visibility'
+abbr --add g git
+abbr --add ge 'git add . && git status && sleep 1 && git diff --cached'
+abbr --add create-remote-repo 'gh repo create --source=. --private --push'
+abbr --add vis 'gh repo view --json visibility -q .visibility'
 ## Copilot
-alias cpe 'gh copilot explain'
-alias cps 'gh copilot suggest'
-### ChatGPT
-alias gpt 'chat --model gpt-4'
+abbr --add cpe 'gh copilot explain'
+abbr --add cps 'gh copilot suggest'
 ## Rust
-alias cg cargo
-alias fmt-all 'cargo +nightly fmt'
-alias re 'echo "Formatting ..." ; fmt-all ; echo "Checking ..." ; cargo check ; echo "Clippying ..." ; cargo clippy'
+abbr --add cg cargo
+abbr --add fmt-all 'cargo +nightly fmt'
+abbr --add re 'echo "Formatting ..." ; fmt-all ; echo "Checking ..." ; cargo check ; echo "Clippying ..." ; cargo clippy'
 ## Tmux
-alias tka 'tmux kill-server'
-alias tls 'tmux ls'
-alias tm 'tmux'
-## Tmuxp
-alias fux 'tmuxp load (tmuxp ls | fzf --layout=reverse --info=inline --height=40%)'
+abbr --add tka 'tmux kill-server'
+abbr --add tls 'tmux ls'
+abbr --add tm 'tmux'
 ## Misc
-alias untar_command 'echo tar xvzf'
-alias nv 'nvim .'
+abbr nv 'nvim .'
 
 if status is-interactive
     # Set vi mode
