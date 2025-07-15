98f98a9dff0fc18aa21809e6f8bb99718c2fdc49
Brendan Nolan <bnolan23@gmail.com>
2025-07-15 21:24:52 +0100

    Add the actual zsh plugins

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -8,3 +8,8 @@ if [ ! -d "$ZINIT_HOME" ]; then
 fi
 
 source "${ZINIT_HOME}/zinit.zsh"
+
+zinit light zsh-users/zsh-syntax-highlighting
+zinit light zsh-users/zsh-completions
+zinit light zsh-users/zsh-autosuggestions
+zinit light Aloxaf/fzf-tab
