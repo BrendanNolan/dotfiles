75024e75b3f065d68cfee14a66c725584d531d7a
Brendan Nolan <bnolan23@gmail.com>
2025-07-17 15:27:12 +0100

    Improve the completion and tab-completion behaviour of zsh

--- .config/zsh/completion_styling.zsh
+++ .config/zsh/completion_styling.zsh
@@ -1,5 +1,5 @@
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
 zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
-zstyle ':completion:*' menu no
-zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
-zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
+zstyle ':completion:*' menu select
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -la --icons --color=always $realpath'
+zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'eza -la --icons --color=always $realpath'
