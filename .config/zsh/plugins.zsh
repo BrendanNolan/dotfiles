242422db747d0db16710dc4c137d6820d5e493ee
Brendan Nolan <bnolan23@gmail.com>
2025-07-21 10:50:05 +0100

    Make clearer ordering of plugin loading

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -22,10 +22,6 @@ install_plugin "fzf-tab" "https://github.com/Aloxaf/fzf-tab"
 install_plugin "zsh-autosuggestions" "https://github.com/zsh-users/zsh-autosuggestions"
 install_plugin "zsh-syntax-highlighting" "https://github.com/zsh-users/zsh-syntax-highlighting.git"
 
-# Autosuggestion Styling
-ZSH_AUTOSUGGEST_STRATEGY=(history completion)
-ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=8'  # light gray
-
 source_zsh_completions() {
 	fpath=(path/to/zsh-completions/src $fpath)
 	if "$FORCE_REBUILD_ZCOMP_DUMP"; then
@@ -39,24 +35,27 @@ source_other_plugin() {
 	source "${ZSH_PLUGIN_HOME}/${PLUGIN_NAME}/${SOURCE_SCRIPT_NAME}"
 }
 
+# Pay attention to the order here. The sourcing, compinit, zstyling, and env var setting are
+# sensitive to the ordering of all of the following lines
+# Completions
 source_zsh_completions
 autoload -Uz compinit && compinit -d "${XDG_CACHE_HOME}/.zcompdump"
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
+zstyle ':completion:*' menu select
+# Fzf-Tab
 source_other_plugin "fzf-tab" "fzf-tab.plugin.zsh"
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -la --icons --color=always $realpath'
+zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'eza -la --icons --color=always $realpath'
+# Autosuggestions
+ZSH_AUTOSUGGEST_STRATEGY=(history completion)
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=8'  # light gray
 source_other_plugin "zsh-autosuggestions" "zsh-autosuggestions.zsh"
+# Syntax Highlighting
 source_other_plugin "zsh-syntax-highlighting" "zsh-syntax-highlighting.zsh"
-
-# Hilight Styling (uses ANSI colour codes)
 ZSH_HIGHLIGHT_STYLES[comment]='fg=8' # light gray
 ZSH_HIGHLIGHT_STYLES[command]='fg=green'
 ZSH_HIGHLIGHT_STYLES[alias]='fg=33' # light blue
 ZSH_HIGHLIGHT_STYLES[builtin]='fg=yellow'
 ZSH_HIGHLIGHT_STYLES[function]='fg=magenta'
 ZSH_HIGHLIGHT_STYLES[unknown-token]='fg=9' # red
-
-# Completion Styling
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
-zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
-zstyle ':completion:*' menu select
-zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -la --icons --color=always $realpath'
-zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'eza -la --icons --color=always $realpath'
-
