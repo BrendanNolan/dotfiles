61ffdaf1e792056cf233f0caf4fa547f45454a1c
Brendan Nolan <bnolan23@gmail.com>
2025-07-21 09:17:36 +0100

    Getting somewhere slowly

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -1,14 +1,6 @@
 # Set the directory where manually loaded plugins will come from
 ZSH_PLUGIN_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zsh_plugins"
 
-# Download Zinit, if it's not there yet
-if [ ! -d "$ZINIT_HOME" ]; then
-	mkdir -p "$(dirname $ZINIT_HOME)"
-	git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
-fi
-
-source "${ZINIT_HOME}/zinit.zsh"
-
 zinit light zsh-users/zsh-syntax-highlighting
 zinit light zsh-users/zsh-completions
 zinit light zsh-users/zsh-autosuggestions
--- .zshrc
+++ .zshrc
@@ -1,12 +1,5 @@
 # Sourced only in interactive shells
 
-source_from_conf() {
-	source "${XDG_CONFIG_HOME:-$HOME/.config}/zsh/${1}.zsh"
-}
-
-source_from_conf "vi"
-source_from_conf "aliases"
-source_from_conf "functions"
-source_from_conf "plugins"
-source_from_conf "shell_integrations"
-source_from_conf "history_opts"
+for subconf in "vi"  "aliases"  "functions"  "plugins"  "shell_integrations"  "history_opts"; do
+	source "${XDG_CONFIG_HOME:-$HOME/.config}/zsh/${subconf}.zsh"
+done
