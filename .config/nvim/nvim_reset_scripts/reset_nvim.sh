3c8503395b435b9dd25d57f5ed300e626b065347
Brendan Nolan <bnolan23@gmail.com>
2025-07-24 16:30:51 +0100

    Minor formatting of some scripts

--- .config/nvim/nvim_reset_scripts/reset_nvim.sh
+++ .config/nvim/nvim_reset_scripts/reset_nvim.sh
@@ -4,4 +4,3 @@ rm -rf ~/.cache/nvim
 rm -rf ~/.local/share/nvim
 rm -rf ~/.local/state/nvim
 rm -rf ~/.config/nvim/plugin
-
--- .config/zsh/vi.zsh
+++ .config/zsh/vi.zsh
@@ -1,8 +1,7 @@
-bindkey -v # vi mode
+bindkey -v   # vi mode
 KEYTIMEOUT=1 # Workaround for very slow entry into normal mode on command line
 
 # Open command line in $EDITOR
 autoload -z edit-command-line
 zle -N edit-command-line
 bindkey '^[e' edit-command-line # alt-e to open command in $EDITOR
-
