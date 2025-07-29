6558f0e9ff578625197d70e37ad596fc3f70e226
Brendan Nolan <bnolan23@gmail.com>
2025-07-29 08:39:54 +0100

    Ensure that fish rc installs fisher and fifc if they are not present

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,12 +4,22 @@ if status is-interactive
     set -gx fifc_editor nvim
 
     source $__fish_config_dir/alias_abbr.fish
+    source $__fish_config_dir/shell_integrations.fish
+
+    if not functions -q fisher
+        echo "Installing fisher ..."
+        curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish \
+            | source && fisher install jorgebucaran/fisher
+    end
+
+    if not contains gazorby/fifc (fisher list)
+        echo "Installing gazorby/fifc"
+        fisher install gazorby/fifc
+    end
 
     fish_vi_key_bindings
 
     bind --user -M insert \ck up-or-search
     bind --user -M insert \cj down-or-search
-
-    source $__fish_config_dir/shell_integrations.fish
 end
 
