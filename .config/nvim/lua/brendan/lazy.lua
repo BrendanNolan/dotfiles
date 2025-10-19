bd0189d07a25dfb4cb4df15824e10fc1f72d757d
Brendan Nolan <bnolan23@gmail.com>
2025-10-19 11:36:18 +0100

    Reinstate outline options

--- .config/nvim/lua/brendan/lazy.lua
+++ .config/nvim/lua/brendan/lazy.lua
@@ -129,13 +129,6 @@ local plugins = {
         "hedyhli/outline.nvim",
         lazy = true,
         cmd = { "Outline", "OutlineOpen" },
-        keys = {
-            { "<leader>m", "<cmd>Outline<CR>", desc = "Toggle outline" },
-        },
-        opts = {},
-        config = function()
-            require("outline").setup({})
-        end,
     },
 }
 
