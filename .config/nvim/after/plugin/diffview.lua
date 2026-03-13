87210c0340d163210be03a76b9e9857b9fa27350
Brendan Nolan <bnolan23@gmail.com>
2026-03-13 10:07:33 +0000

    Add keymaps for diffview.nvim

---
+++ .config/nvim/after/plugin/diffview.lua
@@ -0,0 +1,3 @@
+vim.keymap.set("n", "<leader>dfo", "<cmd>DiffviewOpen<cr>")
+vim.keymap.set("n", "<leader>dfm", "<cmd>DiffviewOpen master..HEAD<cr>")
+vim.keymap.set("n", "<leader>dfc", "<cmd>DiffviewClose<cr>")
