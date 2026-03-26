3064ce84c669a43dbb4270a4577d692348006c6f
Brendan Nolan <bnolan23@gmail.com>
2026-03-26 13:53:31 +0000

    Add keymaps for navigating git conflicts

--- .config/nvim/after/plugin/git.lua
+++ .config/nvim/after/plugin/git.lua
@@ -3,3 +3,7 @@ vim.keymap.set("n", "<leader>gg", vim.cmd.Git);
 vim.keymap.set("n", "<leader>ch", "<cmd>diffget //2<CR>");
 vim.keymap.set("n", "<leader>cl", "<cmd>diffget //3<CR>");
 
+vim.keymap.set("n", "]c", "/^\\(<<<<<<<\\)<CR>", {silent = true, desc = "Next conflict beginning"})
+vim.keymap.set("n", "]c", "/?\\(<<<<<<<\\)<CR>", {silent = true, desc = "Previous conflict beginning"})
+vim.keymap.set("n", "]C", "/^\\(>>>>>>>\\)<CR>", {silent = true, desc = "Next conflict end"})
+vim.keymap.set("n", "]C", "/?\\(>>>>>>>\\)<CR>", {silent = true, desc = "Previous conflict end"})
