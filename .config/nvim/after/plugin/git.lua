7e43de5e7657ee9f549e75614cfaec45240e9884
Brendan Nolan <bnolan23@gmail.com>
2026-03-26 14:17:54 +0000

    Remove unused keymaps

--- .config/nvim/after/plugin/git.lua
+++ .config/nvim/after/plugin/git.lua
@@ -5,7 +5,5 @@ vim.keymap.set("n", "[C", "/?\\(>>>>>>>\\)<CR>", {silent = true, desc = "Previou
 
 -- Fugitive --
 vim.keymap.set("n", "<leader>gg", vim.cmd.Git)
-vim.keymap.set("n", "<leader>ch", "<cmd>diffget //2<CR>")
-vim.keymap.set("n", "<leader>cl", "<cmd>diffget //3<CR>")
 vim.api.nvim_create_user_command("Ours", function() vim.cmd.diffget("//2") end, {})
 vim.api.nvim_create_user_command("Theirs", function() vim.cmd.diffget("//3") end, {})
