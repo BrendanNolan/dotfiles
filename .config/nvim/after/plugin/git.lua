2f17956d63f6262a2328bdc8a5833d3c19d27f2c
Brendan Nolan <bnolan23@gmail.com>
2026-03-26 14:08:15 +0000

    Add keymaps for vim-Fugitive conflict resolution

--- .config/nvim/after/plugin/git.lua
+++ .config/nvim/after/plugin/git.lua
@@ -1,9 +1,11 @@
--- Fugitive --
-vim.keymap.set("n", "<leader>gg", vim.cmd.Git);
-vim.keymap.set("n", "<leader>ch", "<cmd>diffget //2<CR>");
-vim.keymap.set("n", "<leader>cl", "<cmd>diffget //3<CR>");
-
 vim.keymap.set("n", "]c", "/^\\(<<<<<<<\\)<CR>", {silent = true, desc = "Next conflict beginning"})
-vim.keymap.set("n", "]c", "/?\\(<<<<<<<\\)<CR>", {silent = true, desc = "Previous conflict beginning"})
+vim.keymap.set("n", "[c", "/?\\(<<<<<<<\\)<CR>", {silent = true, desc = "Previous conflict beginning"})
 vim.keymap.set("n", "]C", "/^\\(>>>>>>>\\)<CR>", {silent = true, desc = "Next conflict end"})
-vim.keymap.set("n", "]C", "/?\\(>>>>>>>\\)<CR>", {silent = true, desc = "Previous conflict end"})
+vim.keymap.set("n", "[C", "/?\\(>>>>>>>\\)<CR>", {silent = true, desc = "Previous conflict end"})
+
+-- Fugitive --
+vim.keymap.set("n", "<leader>gg", vim.cmd.Git)
+vim.keymap.set("n", "<leader>ch", "<cmd>diffget //2<CR>")
+vim.keymap.set("n", "<leader>cl", "<cmd>diffget //3<CR>")
+vim.api.nvim_create_user_command("Ours", function() vim.cmd.diffget("//2") end, {})
+vim.api.nvim_create_user_command("Theirs", function() vim.cmd.diffget("//3") end, {})
