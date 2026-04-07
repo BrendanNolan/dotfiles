20e0b57d691b5dbc8f0ab49c0e1127ef7e565308
Brendan Nolan <bnolan23@gmail.com>
2026-04-07 08:15:48 +0100

    Increase Wezterm Font Size

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -36,7 +36,7 @@ return {
         },
     },
     font = wezterm.font 'Fira Code',
-    font_size = 18.0,
+    font_size = 21.0,
     hide_tab_bar_if_only_one_tab = true,
     warn_about_missing_glyphs = true,
     cursor_blink_ease_in = 'Constant',
